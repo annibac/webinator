2b793549aeaf3ff7fe06539e9d9bdd2ee4d30285
Tomy <chaboc_t@etna-alternance.net>
2017-12-12 18:58:02 +0100

    upgrade end

--- etna-project/src/app/services/score/score.service.ts
+++ etna-project/src/app/services/score/score.service.ts
@@ -2,13 +2,43 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class ScoreService {
-  value: number;
+  private _value: number;
 
   constructor() {
-    this.value = 0;
+    this._value = 0;
   }
 
+  set value(new_value: number) {
+    this._value = new_value;
+  }
+
+  get value(): number {
+    return this._value;
+  }
+
+  // Update score by 1 (used with score timer)
   update() {
-    return this.value++;
+    this._value++;
+  }
+
+  // Adding value to score value
+  add(adding_value: number) {
+    this._value += adding_value;
+  }
+
+  // Subtracting score value with another value
+  // and check if it's for buying in shop or losing point at project end
+  substract(substrating_value: number, buying: boolean) {
+    if (this._value - substrating_value < 0) {
+      if (buying === false) {
+        return -1;
+      }
+      this._value = 0;
+      return 1;
+    }
+    else {
+      this._value -= substrating_value;
+      return 1;
+    }
   }
 }
